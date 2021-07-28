7b841317cbbeab9124c15debc588d5f107d0b722
Katharine-git <katharinesheen@gmail.com>
2021-07-28 16:08:56 +0530

    edited

--- variables.tf
+++ variables.tf
@@ -1,7 +1,11 @@
 variable vpc_cidr_block{
     default = "172.20.0.0/16"
 }
-variable aws_az{}
-variable aws_ami{}
+variable aws_az{
+    default = "us-east-1"
+}
+variable aws_ami{
+    default = "ami-0dc2d3e4c0f9ebd18"
+}
 variable aws_key_name{}
 variable aws_subnet_id{}
